9bc6a2d08895d071686b888430dd9b1d4595044b
Kholis Ibrohim <kholisibrohim37@gmail.com>
2025-02-27 10:01:04 +0700

    Membuat

---
+++ app/src/main/java/com/example/mysharedpreferences/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.mysharedpreferences
+
+data class UserModel (
+    var name: String? = null,
+    var email: String? = null,
+    var age: Int = 0,
+    var phoneNumber: String? = null,
+    var isLove: Boolean = false
+)
